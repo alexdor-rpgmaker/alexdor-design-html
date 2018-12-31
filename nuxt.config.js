94d5557334a8aa91e0ddfda9664c359709504198
AlexRE <holo.hokkaido@gmail.com>
2018-12-31 09:35:03 +0100

    Préparation pour Github Pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,11 @@
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/alexdor19/'
+  }
+} : {}
+
 module.exports = {
+  ...routerBase,
   /*
   ** Headers of the page
   */
