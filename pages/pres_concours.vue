1a024e85522dd328a8a03f9b504e59a68568c2a4
CyrilRPG <40541959+CyrilRPG@users.noreply.github.com>
2019-01-05 22:46:10 +0100

    Ajout pres_concours

---
+++ pages/pres_concours.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="container">
+    <h2 class = "pres_alex">Présentation du concours</h2><br>
+
+<div class = "pres_bloc">
+	<h3 class = "pres_alex">Les Alex d'Or</h3>
+	<h4>Qu'est ce que c'est ?</h4>
+	<p>
+		Il s'agit d'un concours annuel de création de jeux vidéo, créé en 2001 par Booskaboo. Il a pour but de réunir les jeux créés avec la gamme de logiciels RPG Maker et provenant de toutes les communautés de création de jeu (“RPG Making”) francophones. Il permet aux auteurs de jeux amateurs de comparer leurs talents dans plusieurs domaines (graphismes, programmation, gameplay, histoire, level design, ambiance, mise en scène, etc.). Les meilleurs jeux dans chaque domaine sont sélectionnés par un jury, et annoncés lors d’une cérémonie de fin de session. Des créations artistiques (dessins, musiques, vidéos, etc.) sont réalisées par des artistes bénévoles pour matérialiser la victoire de chaque jeu ; ce sont les “awards”.
+	</p>
+
+	<p>
+		Le concours permet aussi aux créateurs de jeux de bénéficier de critiques constructives et conseils divers, rédigées par les membres du jury, afin de pouvoir améliorer leur création. L’idée est de favoriser le partage de connaissances sur les jeux et le game making.
+	</p>
+
+	<p>
+		Cette compétition cherche donc à motiver tous les créateurs de jeux, et permet de mettre en lumière l'actualité de la communauté RPG Maker francophone chaque année.
+	</p>
+
+	<p>
+		Le concours des Alex d’or se nomme ainsi d'après le héros de base de RPG Maker 2000, qui s'appelle Alex.
+	</p>
+
+	<h4>Comment ça fonctionne ?</h4>
+
+	<p>
+		Le concours est reconduit chaque année, une session de concours se déroulant habituellement entre mars/avril et décembre. Il y a un règlement, plus ou moins de modifications à chaque nouvelle session.
+	</p>
+
+	<p>
+		Pour inscrire son jeu à la session courante, il faut se rendre sur cette page. L'inscription peut aussi servir de "promesse" si le jeu n’a pas encore de lien de téléchargement, à partir du moment où un lien est ajouté avant la date de fin des inscriptions.
+	</p>
+
+	<p>
+		Pour aider l’équipe du concours et faire partie du jury, il faut se rendre sur cette page. De nouveaux jurés sont recrutés chaque année, pour former un jury à chaque fois différent. Chaque juré est amené à rédiger une dizaine de tests, afin d’arriver à un minimum de quatre tests par jeu concurrent. Les tests se font grâce à un template pré-défini où il est possible de noter chaque catégorie compétitive, en accompagnant cette note d’un texte argumentatif. Les meilleurs jeux dans chaque catégorie sont retenus pour une phase de nominations (ayant lieu vers novembre), puis départagés par le jury, après avoir joué à tous les jeux nommés. Les résultats finaux sont annoncés en décembre, lors de la cérémonie de victoire.
+	</p>
+
+	<h4>Qui sont les différents awards ?</h4>
+
+	<p>
+		Chaque concurrent verra son jeu noté dans des catégories compétitives principales (gameplay, histoire, ambiance, graphismes, bande-son), et des catégories dites “spécifiques”, auxquelles il peut s’inscrire en fonction de ce qu’il estime pouvoir gagner. Vous pourrez trouver la liste complète des awards existants de chaque session sur cette page.
+	</p>
+
+	<p>
+		A savoir :
+		<ul>
+			<li>Le jeu qui obtient l’Alex d'or, récompense éponyme, est celui qui est considéré comme le meilleur jeu de l'année.</li>
+			<li>Les awards généraux sont composés de trois "médailles" (Or, Argent et Bronze), contrairement aux awards spécifiques où il n'y a qu'une seule et unique récompense par award.</li>
+			<li>Les Coups de coeur (du public et du jury) sont désignés par votes en toute fin de session. Le Coup de coeur du jury récompense un jeu qui n'a été récompensé nulle part, mais que le jury estime mériter d’être mis en avant.</li>
+			<li>Il est possible que d'autres awards bonus soient créés lors des délibérations, en fonction des tendances de chaque session. Par exemple : "Meilleurs minis-jeux", "Meilleur jeu d’action" ou bien "Meilleur Espoir". Ce dernier award est souvent décerné à un projet assez jeune, dont le début est prometteur et qui mérite d'être encouragé.</li>
+		</ul>
+	</p>
+
+	<h4>En bref</h4>
+
+	<p>
+		L'objectif du concours est d'offrir une compétition amusante et conviviale mais néanmoins réalisée avec sérieux ! Cette organisation est totalement bénévole et, chaque année, nécessite des personnes volontaires pour y participer, que ce soit en tant que concurrent, juré ou bien encore organisateur. A vous de jouer, donc, pour assurer la pérennité et des sessions futures pleines de réussite !
+	</p>
+
+	<p>
+		Bonne visite à tous et bon concours des Alex d'or !
+	</p>
+	
+</div>
+</div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
